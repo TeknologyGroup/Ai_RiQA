8e7ff2165a4b2700b78759b8e53b8d09cb8ee94b
Martyn Battista <109523724+TeknologyGroup@users.noreply.github.com>
2025-04-05 16:08:25 +0200

    Create QuantumPage.vue

---
+++ frontend/src/views/QuantumPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="quantum-page p-6">
+    <h1 class="text-2xl font-bold text-blue-600">Quantum Simulations</h1>
+    <QuantumLab />
+    <SimulationControls @update="updateParam" />
+  </div>
+</template>
+
+<script>
+import QuantumLab from '../components/QuantumLab.vue';
+import SimulationControls from '../components/SimulationControls.vue';
+
+export default {
+  name: 'QuantumPage',
+  components: {
+    QuantumLab,
+    SimulationControls
+  },
+  methods: {
+    updateParam(value) {
+      console.log('Parameter updated:', value);
+    }
+  }
+};
+</script>
